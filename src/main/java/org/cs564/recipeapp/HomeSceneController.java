a8db0b8716061457548fac2f12d949a93512b882
alonergan <alonergan@wisc.edu>
2022-07-25 18:05:35 -0500

    Started search feature. Added a few scenes and functions, but still need to figure out an algorithm for searching recipes via SQL. Not sure how to handle the immense amount of data and minute differences between ingredients

--- src/main/java/org/cs564/recipeapp/HomeSceneController.java
+++ src/main/java/org/cs564/recipeapp/HomeSceneController.java
@@ -1,26 +1,35 @@
 package org.cs564.recipeapp;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
-
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import java.net.URL;
 import java.sql.*;
+import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class HomeSceneController {
+
+    // SCENE BUILDER CODE /////////////////////////////////////////////////////////////////////////////////////////////
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private RadioButton beefRadioButton;
 
     @FXML
     private Pane browsePane;
@@ -29,28 +38,85 @@ public class HomeSceneController {
     private Button browseRecipeButton;
 
     @FXML
-    private TableColumn<TableModel, String> dateCol;
+    private CheckBox carrotCheckBox;
+
+    @FXML
+    private RadioButton chickenRadioButton;
+
+    @FXML
+    private CheckBox cornCheckBox;
+
+    @FXML
+    private TableColumn<?, ?> dateCol;
+
+    @FXML
+    private RadioButton eggsRadioButton;
+
+    @FXML
+    private RadioButton fishRadioButton;
+
+    @FXML
+    private CheckBox garlicCheckBox;
 
     @FXML
     private Button homeButton;
 
     @FXML
-    private TableColumn<TableModel, Integer> ingredCol;
+    private TableColumn<?, ?> ingredCol;
 
     @FXML
     private Button logoutButton;
 
     @FXML
-    private TableColumn<TableModel, Integer> minutesCol;
+    private TableColumn<?, ?> minutesCol;
 
     @FXML
-    private TableColumn<TableModel, String> nameCol;
+    private TableColumn<?, ?> nameCol;
+
+    @FXML
+    private Button nextPageButton;
+
+    @FXML
+    private CheckBox onionCheckBox;
+
+    @FXML
+    private TextField pageNumber;
+
+    @FXML
+    private CheckBox pastaCheckBox;
+
+    @FXML
+    private CheckBox pepperCheckBox;
+
+    @FXML
+    private CheckBox potatoCheckBox;
+
+    @FXML
+    private Button prevPageButton;
 
     @FXML
     private Pane profilePane;
 
     @FXML
+    private ToggleGroup proteinGroup;
+
+    @FXML
+    private Button quitButton;
+
+    @FXML
+    private CheckBox riceCheckBox;
+
+    @FXML
+    private AnchorPane searchPane1;
+
+    @FXML
     private Button searchRecipeButton;
+
+    @FXML
+    private Button searchSubmitButton1;
+
+    @FXML
+    private Button searchSubmitButton2;
 
     @FXML
     private Button settingsButton;
@@ -59,19 +125,58 @@ public class HomeSceneController {
     private Pane settingsPane;
 
     @FXML
-    private TableColumn<TableModel, Integer> stepsCol;
+    private CheckBox spinachCheckBox;
+
+    @FXML
+    private TableColumn<?, ?> stepsCol;
 
     @FXML
     private TableView<TableModel> table;
 
     @FXML
-    private Button nextPageButton;
+    private CheckBox tomatoCheckBox;
 
     @FXML
-    private Button prevPageButton;
+    void initialize() {
+        assert beefRadioButton != null : "fx:id=\"beefRadioButton\" was not injected: check your FXML file 'homeSceneController.fxml'.";
+        assert browsePane != null : "fx:id=\"browsePane\" was not injected: check your FXML file 'homeSceneController.fxml'.";
+        assert browseRecipeButton != null : "fx:id=\"browseRecipeButton\" was not injected: check your FXML file 'homeSceneController.fxml'.";
+        assert carrotCheckBox != null : "fx:id=\"carrotCheckBox\" was not injected: check your FXML file 'homeSceneController.fxml'.";
+        assert chickenRadioButton != null : "fx:id=\"chickenRadioButton\" was not injected: check your FXML file 'homeSceneController.fxml'.";
+        assert cornCheckBox != null : "fx:id=\"cornCheckBox\" was not injected: check your FXML file 'homeSceneController.fxml'.";
+        assert dateCol != null : "fx:id=\"dateCol\" was not injected: check your FXML file 'homeSceneController.fxml'.";
+        assert eggsRadioButton != null : "fx:id=\"eggsRadioButton\" was not injected: check your FXML file 'homeSceneController.fxml'.";
+        assert fishRadioButton != null : "fx:id=\"fishRadioButton\" was not injected: check your FXML file 'homeSceneController.fxml'.";
+        assert garlicCheckBox != null : "fx:id=\"garlicCheckBox\" was not injected: check your FXML file 'homeSceneController.fxml'.";
+        assert homeButton != null : "fx:id=\"homeButton\" was not injected: check your FXML file 'homeSceneController.fxml'.";
+        assert ingredCol != null : "fx:id=\"ingredCol\" was not injected: check your FXML file 'homeSceneController.fxml'.";
+        assert logoutButton != null : "fx:id=\"logoutButton\" was not injected: check your FXML file 'homeSceneController.fxml'.";
+        assert minutesCol != null : "fx:id=\"minutesCol\" was not injected: check your FXML file 'homeSceneController.fxml'.";
+        assert nameCol != null : "fx:id=\"nameCol\" was not injected: check your FXML file 'homeSceneController.fxml'.";
+        assert nextPageButton != null : "fx:id=\"nextPageButton\" was not injected: check your FXML file 'homeSceneController.fxml'.";
+        assert onionCheckBox != null : "fx:id=\"onionCheckBox\" was not injected: check your FXML file 'homeSceneController.fxml'.";
+        assert pageNumber != null : "fx:id=\"pageNumber\" was not injected: check your FXML file 'homeSceneController.fxml'.";
+        assert pastaCheckBox != null : "fx:id=\"pastaCheckBox\" was not injected: check your FXML file 'homeSceneController.fxml'.";
+        assert pepperCheckBox != null : "fx:id=\"pepperCheckBox\" was not injected: check your FXML file 'homeSceneController.fxml'.";
+        assert potatoCheckBox != null : "fx:id=\"potatoCheckBox\" was not injected: check your FXML file 'homeSceneController.fxml'.";
+        assert prevPageButton != null : "fx:id=\"prevPageButton\" was not injected: check your FXML file 'homeSceneController.fxml'.";
+        assert profilePane != null : "fx:id=\"profilePane\" was not injected: check your FXML file 'homeSceneController.fxml'.";
+        assert proteinGroup != null : "fx:id=\"proteinGroup\" was not injected: check your FXML file 'homeSceneController.fxml'.";
+        assert quitButton != null : "fx:id=\"quitButton\" was not injected: check your FXML file 'homeSceneController.fxml'.";
+        assert riceCheckBox != null : "fx:id=\"riceCheckBox\" was not injected: check your FXML file 'homeSceneController.fxml'.";
+        assert searchPane1 != null : "fx:id=\"searchPane1\" was not injected: check your FXML file 'homeSceneController.fxml'.";
+        assert searchRecipeButton != null : "fx:id=\"searchRecipeButton\" was not injected: check your FXML file 'homeSceneController.fxml'.";
+        assert searchSubmitButton1 != null : "fx:id=\"searchSubmitButton1\" was not injected: check your FXML file 'homeSceneController.fxml'.";
+        assert settingsButton != null : "fx:id=\"settingsButton\" was not injected: check your FXML file 'homeSceneController.fxml'.";
+        assert settingsPane != null : "fx:id=\"settingsPane\" was not injected: check your FXML file 'homeSceneController.fxml'.";
+        assert spinachCheckBox != null : "fx:id=\"spinachCheckBox\" was not injected: check your FXML file 'homeSceneController.fxml'.";
+        assert stepsCol != null : "fx:id=\"stepsCol\" was not injected: check your FXML file 'homeSceneController.fxml'.";
+        assert table != null : "fx:id=\"table\" was not injected: check your FXML file 'homeSceneController.fxml'.";
+        assert tomatoCheckBox != null : "fx:id=\"tomatoCheckBox\" was not injected: check your FXML file 'homeSceneController.fxml'.";
 
-    @FXML
-    private TextField pageNumber;
+    }
+
+    // MY CODE ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     ObservableList<TableModel> oblist = FXCollections.observableArrayList();
     ObservableList<TableModel> curPage = FXCollections.observableArrayList();
@@ -80,19 +185,6 @@ public class HomeSceneController {
     private int maxPages;
     Connection connection;
     ResultSet rs;
-
-
-    @FXML
-    void initialize() {
-        assert browsePane != null : "fx:id=\"browsePane\" was not injected: check your FXML file 'homeSceneController.fxml'.";
-        assert browseRecipeButton != null : "fx:id=\"browseRecipeButton\" was not injected: check your FXML file 'homeSceneController.fxml'.";
-        assert homeButton != null : "fx:id=\"homeButton\" was not injected: check your FXML file 'homeSceneController.fxml'.";
-        assert logoutButton != null : "fx:id=\"logoutButton\" was not injected: check your FXML file 'homeSceneController.fxml'.";
-        assert profilePane != null : "fx:id=\"profilePane\" was not injected: check your FXML file 'homeSceneController.fxml'.";
-        assert searchRecipeButton != null : "fx:id=\"searchRecipeButton\" was not injected: check your FXML file 'homeSceneController.fxml'.";
-        assert settingsButton != null : "fx:id=\"settingsButton\" was not injected: check your FXML file 'homeSceneController.fxml'.";
-        assert settingsPane != null : "fx:id=\"settingsPane\" was not injected: check your FXML file 'homeSceneController.fxml'.";
-    }
 
     public void handleClicks(ActionEvent event) throws Exception {
         if (event.getSource() == homeButton) {
@@ -105,6 +197,9 @@ public class HomeSceneController {
             browsePane.toFront();
             browseInitialize();
         }
+        if (event.getSource() == searchRecipeButton) {
+            searchPane1.toFront();
+        }
         if (event.getSource() == logoutButton) {
             Parent scene = FXMLLoader.load(getClass().getResource("loginSceneController.fxml"));
             Stage stage = (Stage) logoutButton.getScene().getWindow();
@@ -116,13 +211,13 @@ public class HomeSceneController {
 
         try {
             int count = 0;
-            String sqlStatement = "SELECT recipeName, minutes, n_steps, n_ingredients, submitted FROM recipe";
+            String sqlStatement = "SELECT recipe_name, minutes, n_steps, n_ingredients, submitted FROM recipe";
             connection = DatabaseConnector.getConnection();
             rs = connection.createStatement().executeQuery(sqlStatement);
 
             // Add all contents to oblist for storage
             while (rs.next()) {
-                oblist.add(new TableModel(rs.getString("recipeName"), rs.getInt("minutes"), rs.getInt("n_steps"), rs.getInt("n_ingredients"), rs.getString("submitted")));
+                oblist.add(new TableModel(rs.getString("recipe_name"), rs.getInt("minutes"), rs.getInt("n_steps"), rs.getInt("n_ingredients"), rs.getString("submitted")));
             }
 
             // Set pages and update table
@@ -161,6 +256,36 @@ public class HomeSceneController {
         }
     }
 
+    /*
+     * Search will work as follows (Still working this out...)
+     * Step 1: Select a basic set of options to narrow down the immense amount of recipes
+     * Step 2: From the sub-table created in step 1 create a list of check boxes for ingredients
+     * Step 3: Show results?
+     */
+    @FXML
+    public void handleSearchSubmit(ActionEvent event) {
+        // Step 1
+        if (event.getSource() == searchSubmitButton1) {
+            // Get selections
+            // Get subtable of ingredients from recipes containing selections
+            // Bring searchPane2 to front
+            // Display other ingredients as group of check boxes
+            return;
+        }
+        // Step 2 + 3 TODO
+        if (event.getSource() == searchSubmitButton2) {
+            // Get subtable of recipes containing all selections
+            // Bring searchPane3 to front
+            // Display subtable of recipes containing ingredients ordered ascending by the num of ingredients you have
+            return;
+        }
+    }
+
+    @FXML
+    public void quitApplication() {
+        Platform.exit();
+    }
+
     private void updatePage(int pageIndex) throws SQLException {
         if (oblist.size() == 0) {
             System.out.println("oblist empty");
@@ -187,4 +312,5 @@ public class HomeSceneController {
         table.setItems(curPage);
         pageNumber.setPromptText(String.valueOf(pageIndex));
     }
+
 }
--- src/main/java/org/cs564/recipeapp/Ingredient.java
+++ src/main/java/org/cs564/recipeapp/Ingredient.java
@@ -1,4 +1,19 @@
 package org.cs564.recipeapp;
 
 public class Ingredient {
+    private int recipeID;
+    private String name;
+
+    public Ingredient(int recipeID, String name) {
+        this.recipeID = recipeID;
+        this.name = name;
+    }
+
+    public int getRecipeID() {
+        return recipeID;
+    }
+
+    public void setRecipeID(int recipeID) {
+        this.recipeID = recipeID;
+    }
 }
